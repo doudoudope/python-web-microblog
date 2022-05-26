3c1336d0fdb0dd439bc79533068a6aac73c3edaa
Ye <93702323+doudoudope@users.noreply.github.com>
2022-05-25 21:58:58 -0700

    Add files via upload
    second commit

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 import datetime
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 
+
 def create_app():
     app = Flask(__name__)
-    client = MongoClient("mongodb+srv://sarahzy605:jz199165@microblog-app.xixib.mongodb.net/test")
+    client = MongoClient(os.environ.get("MONGODB_URI"))
     app.db = client.microblog
 
     @app.route("/", methods=["GET", "POST"])
@@ -12,7 +14,7 @@ def create_app():
         if request.method == "POST":
             entry_content = request.form.get("content")
             formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+            app.db.entries.insert({"content": entry_content, "date": formatted_date})
         
         entries_with_date = [
             (
